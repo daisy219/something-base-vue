e607ecc5863ae192902339eab6e38f6d0c67f70c
daisy219 <daisyzhao219@outlook.com>
2021-03-15 13:54:14 +0800

    eltable合并单å格

--- src/pages/email/email.vue
+++ src/pages/email/email.vue
@@ -7,6 +7,15 @@
       :span-method="arraySpanMethod"
       default-expand-all
       :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
+      <el-table-column width="80" align="center">
+        <template slot="header" slot-scope="scope">
+          <el-checkbox v-model="tableSelectAll" :indeterminate="halfSelectAll" @change="(val) => selectAllChange(val, scope)"/>
+        </template>
+        <template slot-scope="scope">
+          <el-checkbox v-model="scope.row.select" v-if="!scope.row.children" @change="(val) => selectSingleChange(val, scope)"/>
+        </template>
+      </el-table-column>
+
       <el-table-column
         prop="name"
         label="姓名"
@@ -31,6 +40,8 @@
 export default {
   data() {
     return {
+      tableSelectAll: false,
+      halfSelectAll: false,
       tableData: [
         {
           id: 3,
@@ -64,13 +75,34 @@ export default {
             content: '上海市普陀区金沙江路 1519 弄'
           }]
         },
-    ]
-  }},
+      ]
+    }
+  },
+  computed: {
+    childrenList() {
+      return this.tableData.map(a => a.children).flat(2);
+    },
+  },
   methods: {
+    /** 全选变更 */
+    selectAllChange(val) {
+      this.childrenList.forEach(item => {
+        this.$set(item, 'select', val);
+      })
+    },
+    /** 单个选择变更 */
+    selectSingleChange(val, scope) {
+      console.log(scope);
+      const length = this.childrenList.filter(item => item.select).length;
+      this.tableSelectAll = length === this.childrenList.length ? true : false;
+      this.halfSelectAll = length !== 0 && !this.tableSelectAll ? true : false;
+    },
     arraySpanMethod({ row, column, rowIndex, columnIndex }) {
       if (row.children) {
-        if (columnIndex === 0) {
-          return [1, 3];
+        if (columnIndex === 1) {
+          return [1, 4];
+        } else if (columnIndex === 0) {
+          return [1, 1];
         } else {
           return [0, 0]
         }
